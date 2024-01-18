1c21a9e4ba0c96c8fd721bf7ab83d6f13630b42a
BasedLenin <lotromen@gmail.com>
2024-01-17 21:24:48 -0700

    Figuring out how to open a file was very annoying.

--- 2015/elixir/day01.exs
+++ 2015/elixir/day01.exs
@@ -1,1 +1,10 @@
-ok
+defmodule Day01 do
+  def data do
+    Path.expand("aoc/2015/data/day01.txt", "~/projects")
+    |> File.read!()
+    |> String.split("", trim: true)
+  end
+
+  def sum do
+  end
+end
