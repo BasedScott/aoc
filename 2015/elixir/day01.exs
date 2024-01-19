7936f4dba246fcae04988614019153d751185691
BasedLenin <lotromen@gmail.com>
2024-01-18 18:36:20 -0700

    idk why String.to_integer() dosnt work.

--- 2015/elixir/day01.exs
+++ 2015/elixir/day01.exs
@@ -4,7 +4,21 @@ defmodule Day01 do
     |> File.read!()
     |> String.split("", trim: true)
   end
+end
 
-  def sum do
+defmodule Convert do
+  def convert([]), do: 0
+
+  def convert([head | tail]) do
+    x =
+      if head == "(" do
+        String.replace(head, "(", "1")
+        |> String.to_integer()
+        |> IO.puts()
+      else
+        String.replace(head, ")", "-1", global: true)
+      end
+
+    convert(tail)
   end
 end
