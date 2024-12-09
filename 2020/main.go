bdf2d175262ccb7a28ff0ec193c2f87b80d1641f
scott <lotromen@gmail.com>
2024-12-08 21:25:47 -0800

    memes

--- 2020/main.go
+++ 2020/main.go
@@ -4,73 +4,31 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"slices"
-	"strconv"
+	"strings"
 )
 
-var intslice []int
-
 func main() {
-	data()
-	part1()
-	part2()
+	data, _ := os.ReadFile("data/day02.txt")
+	list := bytes.Split(data, []byte("\n"))
+	convert(list)
 }
 
-func part2() int {
-	d := slices.Clone(intslice)
-	p := slices.Clone(intslice)
-loop1:
-	for {
-		for k, i := range d {
-			// fmt.Println("start:", k, "i:", i, "d[0]:", d[0], "p[0]:", p[0], "total:", i+d[0]+p[0])
-			if i+d[0]+p[0] == 2020 {
-				ok, _ := fmt.Println("Part2 :", i*d[0]*p[0], i, d[0], p[0])
-				return ok
-			} else if k == len(d)-1 {
-				p = slices.Delete(p, 0, 1)
-				// fmt.Println("first statement")
-				continue loop1
-			} else if len(p) == 1 {
-				d = slices.Delete(d, 0, 1)
-				p = slices.Clone(intslice)
-				// fmt.Println("second statement")
-				continue loop1
-			}
-			// fmt.Println("end:", k, "\n", "i:", i, "d[0]:", d[0], "p[0]:", p[0], "total:", i+d[0]+p[0])
-		}
-		fmt.Println("stop")
-		break
-	}
-	return 0
-}
+// func part1(bss [][]byte) int {
+// 	return 0
+// }
 
-func part1() int {
-	d := slices.Clone(intslice)
-loop:
-	for {
-		for k, i := range d {
-			if i+d[0] == 2020 {
-				ok, _ := fmt.Println("Part1 :", i*d[0], i, d[0])
-				return ok
-			} else if k == len(d)-1 {
-				d = slices.Delete(d, 0, 1)
-				continue loop
-			}
-		}
-		break
+func convert(bss [][]byte) string {
+	// var CONTAIN string
+	var MIN string
+	// var MAX int
+	var PW string
+	// var DEBUG string
+	for _, bs := range bss {
+		s := string(bs)
+		_, PW, _ := strings.Cut(s, ": ")
+		MIN, _, _ := strings.Cut(s, "-")
+		fmt.Println("inside statement", "PW", PW, "MIN", MIN)
 	}
-	return 0
-}
-
-func data() []int {
-	data, _ := os.ReadFile("data/day01.txt")
-	line := bytes.Split(data, []byte("\n"))
-	for _, num := range line {
-		i, _ := strconv.Atoi(string(num))
-		if i == 0 {
-		} else {
-			intslice = append(intslice, i)
-		}
-	}
-	return intslice
+	fmt.Println("outside statement", PW, MIN)
+	return "ok"
 }
---
+++ 2024/go.mod
@@ -0,0 +1,3 @@
+module year_2024
+
+go 1.23.2
